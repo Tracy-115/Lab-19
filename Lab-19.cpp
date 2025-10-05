293b13126a16c69b2da09ae84bb8960d5fbb6feb
Tracy-115 <kmin984@insite.4cd.edu>
2025-10-05 16:00:04 -0700

    Update Lab-19.cpp

--- Lab-19.cpp
+++ Lab-19.cpp
@@ -1,1 +1,25 @@
+// COMSC-210 | Lab 19 | Kyawt Thinzar Min
+// IDE used: Visual Studio Code
 
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+class Movie{
+private:
+    string title;
+    Review *head;
+public: 
+    Movie(string t) : title(t), head(nullptr) {}
+    ~Movie() {
+        while (head) {
+            Review *temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
